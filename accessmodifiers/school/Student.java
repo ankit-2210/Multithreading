985db84b33362c15d05fd075ed906e784f7f69ad
Ankit Agarwal <78948475+ankit-2210@users.noreply.github.com>
2026-04-27 11:04:43 +0530

    Add files via upload

---
+++ accessmodifiers/school/Student.java
@@ -0,0 +1,19 @@
+package accessmodifiers.school;
+
+public class Student {
+    public String name;
+    public int age;
+
+    public Student(){
+
+    }
+
+    public void sayHello() {
+        System.out.println("Hello");
+    }
+
+    public static void sayBye(){
+        System.out.println("Say Bye");
+    }
+
+}
