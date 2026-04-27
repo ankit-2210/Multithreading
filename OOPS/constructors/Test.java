ada153bfd8a5c5ce1b731c87d2146c5facbc6352
Ankit Agarwal <78948475+ankit-2210@users.noreply.github.com>
2026-04-27 11:31:39 +0530

    Add files via upload

---
+++ OOPS/constructors/Test.java
@@ -0,0 +1,10 @@
+package constructors;
+
+public class Test {
+    public static void main(String[] args){
+        Student student = new Student();  // default constructors
+        System.out.println(student.getAge());
+
+
+    }
+}
